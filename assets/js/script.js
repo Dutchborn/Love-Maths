4e139ff0f1afecfaa582d65772ef0f7e028a25b3
Dutchborn <lesleyasibey@gmail.com>
2025-05-06 13:39:34 +0100

    added code for subtraction and division

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,6 +31,10 @@ function runGame(gameType) {
     displayAdditionQuestion(num1, num2);
    } else if (gameType === "multiply") {
       displayMultiplyQuestion(num1, num2);
+    } else if (gameType === "subtract") {
+      displaySubtractQuestion(num1, num2);
+    } else if (gameType === "division") {
+      displayDivisionQuestion(num1, num2);
     } else {
       alert(`Unknown game type: ${gameType}`);
       throw `Unknown game type: ${gameType}. Aborting!`; 
@@ -72,6 +76,10 @@ function calculateCorrectAnswer() {
     return [operand1 + operand2, 'addition'];
   } else if (operator === "x") {
     return [operand1 * operand2, 'multiply'];
+  } else if (operator === "-") {
+    return [operand1 - operand2, 'subtract'];
+  } else if (operator === "/") {
+    return [operand1 / operand2, 'division'];
   } else {
     alert(`Unknown operator: ${operator}`);
     throw `Unknown operator: ${operator}. Aborting!`;
@@ -102,7 +110,9 @@ function displayAdditionQuestion(operand1, operand2) {
 }
 
 function displaySubtractQuestion(operand1, operand2) {
-
+  document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+  document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+  document.getElementById("operator").textContent = "-";
 }
 
 function displayMultiplyQuestion(operand1, operand2) {
@@ -112,6 +122,8 @@ function displayMultiplyQuestion(operand1, operand2) {
   document.getElementById("operator").textContent = "x";
 }
 
-function displayDivisionQuestion() {
-
+function displayDivisionQuestion(operand1, operand2) {
+  document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+  document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+  document.getElementById("operator").textContent = "/";
 }
